94525c893fdf6bdb91a188054999ab4e7e0738c5
EdoCirone <edoardocirone@gmail.com>
2025-06-05 14:36:35 +0200

    ho aggiunto il CameraFollow

---
+++ Assets/Scripts/Camera_script/CameraFollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform _target; // Il target da seguire, ad esempio il giocatore
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (_target != null) // Controlla se il target è stato assegnato
+        {
+            Vector3 newPosition = _target.position; // Prende la posizione del target
+
+            newPosition.z = transform.position.z; // Mantiene la stessa profondità della camera
+
+            transform.position = newPosition; // Aggiorna la posizione della camera
+        }
+
+        else
+        {
+            transform.position = new Vector3(0f, 0f, transform.position.z);  // Se il target non è assegnato, imposta la posizione della camera al centro della scena
+        }
+
+    }
+}
